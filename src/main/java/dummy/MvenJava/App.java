f76162ee40bbd282d5b22345dc028b1aeca49390
sravy <sravya.vpk@gmail.com>
2020-11-02 15:36:56 -0500

    first commit

---
+++ src/main/java/dummy/MvenJava/App.java
@@ -0,0 +1,13 @@
+package dummy.MvenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
